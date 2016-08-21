659d8e181e52a8067bc60ab1eff217a38eae6703
Harshad Sharma <harshad@sharma.io>
2016-08-22 01:20:21 +0530

    Removing build.sh, since the `pybble` CLI is better with cross-platform clipboard copy.

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "[pybble] Preparing..."
-transcrypt -p .none app.py
-echo "[pybble] Copying app to app.js..."
-cat __javascript__/app.min.js > app.js
-echo "[pybble] Done."
