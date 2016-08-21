d5899dfb92a67a51f370b506f9655587fa968f97
Harshad Sharma <harshad@sharma.io>
2016-08-21 22:20:49 +0530

    Do web sockets work? Seems they're connecting.

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from pybble import console, UI
+from pybble import console, UI, websocket
+
+
+def test_websock():
+    console.log('creating websocket')
+    socket = websocket('ws://192.168.1.204:8000/chat/')
+    socket.onopen = lambda e: console.log(
+        'Connected. {}'.format(e))
+    socket.onerror = lambda e: console.log(
+        'Unable to connect. Error: {}'.format(e))
 
 
 def on_click_up(event):
@@ -14,7 +23,7 @@ home = UI.Card(title='Home')  # Initialize the card
 home.body('Use it (almost) like they show in Pebble.js API :)')
 
 # A regular function as callback for click
-home.on('click', 'up', on_click_up)
+home.on('click', 'up', test_websock)
 
 # A nameless function/ lambda for callback
 home.on('click', 'select', lambda e: console.log('Select was clicked.'))
--- pybble/__init__.py
+++ pybble/__init__.py
@@ -1,2 +1,7 @@
 def js_require(lib):
     return require(lib)
+
+
+def websocket(url):
+    return __new__(WebSocket(url))
+
