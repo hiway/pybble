82a75ebaacc62470932f42417aebfcf284311dfe
Harshad Sharma <harshad@sharma.io>
2016-08-21 05:44:19 +0530

    A nicer demo app, with more comments.

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 from pybble import console, UI
 
-home = UI.Card(title='Home')
+
+def on_click_up(event):
+    """
+    A normal function for callback.
+    """
+    console.log('Up was clicked.')
+
+
+home = UI.Card(title='Home')  # Initialize the card
+
+# This works, because at runtime, this *is* Javascript
+home.body('Use it (almost) like they show in Pebble.js API :)')
+
+# A regular function as callback for click
+home.on('click', 'up', on_click_up)
+
+# A nameless function/ lambda for callback
 home.on('click', 'select', lambda e: console.log('Select was clicked.'))
-home.show()
+
+home.show()  # Show the Card
