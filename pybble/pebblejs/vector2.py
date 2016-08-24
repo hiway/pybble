121d10aa6a712f343755b262a99f33c37ab24a03
Harshad Sharma <harshad@sharma.io>
2016-08-24 18:53:44 +0530

    Fixing import issues. Currently, you will have to import it as `from pybble.pebblejs.vector2 import Vector2` (shortening the chain is fraught with JS failures)

--- pybble/pebblejs/vector2.py
+++ pybble/pebblejs/vector2.py
@@ -1,8 +1,4 @@
-_vector2 = require('vector2')
+__VECTOR2 = require('vector2')
 
-__pragma__('kwargs')
-
-def Vector2(**kwargs):
-    return __new__(_vector2, kwargs)
-
-__pragma__('nokwargs')
+def Vector2(x, y):
+    return __new__(__VECTOR2(x, y))
